83dbc74e965b871b538e39680b095f3bf6dcc12a
Unknown <alexandrei1998@hotmail.it>
2019-04-26 10:13:14 -0500

    updated clean version

--- data/attributions.js
+++ data/attributions.js
@@ -6,18 +6,10 @@ module.exports = [{
   image: 'img/icon/directory.png',
   artist: 'dtafalonso',
   link: 'http://www.iconarchive.com/show/yosemite-flat-icons-by-dtafalonso/Folder-icon.html',
-}, {
-  image: 'img/icon/attribution.png',
-  artist: 'Custom Icon Design',
-  link: 'http://www.iconarchive.com/show/flatastic-7-icons-by-custom-icon-design/License-manager-icon.html',
-}, {
+},{
   image: 'img/icon/terminal.png',
   artist: 'paomedia',
   link: 'http://www.iconarchive.com/show/small-n-flat-icons-by-paomedia/terminal-icon.html',
-}, {
-  image: 'img/icon/version_history.png',
-  artist: 'Hamza Saleem',
-  link: 'http://www.iconarchive.com/show/stock-icons-by-hamzasaleem/Time-Machine-icon.html',
 }, {
   image: 'img/wallpaper.jpg',
   artist: 'Derrick Brutel',
